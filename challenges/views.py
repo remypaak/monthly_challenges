6582fc40a8507c610492d92ca4cd7d4a605ad116
Remy Paak <rpaak@ohpen.com>
2022-11-11 16:33:57 +0100

    WIP

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from helpers.better_enum import BetterEnum
-from django.http import HttpRequest, HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import (
+    HttpRequest,
+    HttpResponse,
+    HttpResponseNotFound,
+    HttpResponseRedirect,
+)
 from django.urls import reverse
 
 
@@ -8,27 +13,38 @@ class MonthlyChallenges(BetterEnum):
     january = 'Eat meat'
     february = 'Walk a lot'
     march = 'Gym a lot'
-    april = 'Eat meat'
-    may = 'Walk a lot'
-    june = 'Gym a lot'
-    july = 'Eat meat'
-    august = 'Walk a lot'
-    september = 'Gym a lot'
-    october = 'Eat meat'
-    november = 'Walk a lot'
-    december = 'Gym a lot'
+    april = 'Read a lot'
+    may = 'Run a lot'
+    june = 'Relax a lot'
+    july = 'Take a lot of sauna'
+    august = 'Study a lot'
+    september = 'No meat month'
+    october = 'Stoptober'
+    november = 'No nut November'
+    december = 'Cheat month'
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    html_list_item = ''
+    for month in MonthlyChallenges:
+        html_list_item += f'<li><a href="{reverse("monthly-challenge", args=[month.name])}">{month.name.capitalize()}</a></li>'
+    return HttpResponse(f'<ul>{html_list_item}</ul>')
 
 
 def monthly_challenge_by_number(request: HttpRequest, month: int):
     for index, member in enumerate(MonthlyChallenges):
         if index == (month - 1):
-            return HttpResponseRedirect(reverse("monthly-challenge", args=[member.name]))
-    return HttpResponseNotFound("No month is represented by that number")
+            return HttpResponseRedirect(
+                reverse("monthly-challenge", args=[member.name])
+            )
+    return HttpResponseNotFound(
+        "<h1>No month is represented by that number</h1>"
+    )
 
 
 def monthly_challenges(request: HttpRequest, month: str) -> HttpResponse:
     try:
-        return HttpResponse(MonthlyChallenges.from_name(month).value)
+        challenge_text = f'<h1>{MonthlyChallenges.from_name(month).value}</h1>'
+        return HttpResponse(challenge_text)
     except ValueError:
-        return HttpResponseNotFound("No month with that name exists")
-
+        return HttpResponseNotFound("<h1>No month with that name exists</h1>")
